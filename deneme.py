55cac3a11e7f0b83c5e4a86d8545641de56b92a5
Kadir Engin <kadirengin624@hotmail.com>
2022-12-28 23:31:54 +0300

    initial commit

---
+++ deneme.py
@@ -0,0 +1,61 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+import cv2
+from PIL import Image
+import random
+import pickle
+
+dataPath = "myData"
+df = pd.read_csv("labels.csv")
+categories = []
+
+values = df.iloc[:,0].values
+
+for value in values:
+  categories.append(str(value))
+
+trainingData = []
+imgSize = 32
+
+def createTrainingData():
+  i = 0
+  for category in categories:
+    path = os.path.join(dataPath, category)
+    classNum = categories.index(category)
+    for img in os.listdir(path):
+      i += 1
+      print(i)
+      try:
+        img_array = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)
+        newArray = cv2.resize(img_array, (imgSize, imgSize))
+        trainingData.append([newArray, classNum])
+      except Exception as e:
+        pass
+
+createTrainingData()
+
+random.shuffle(trainingData)
+
+X = []
+y = []
+
+for features, label in trainingData:
+  X.append(features)
+  y.append(label)
+
+X = np.array(X).reshape(-1, imgSize, imgSize, 1)
+
+pickleOut = open("X.pickle", "wb")
+pickle.dump(X, pickleOut)
+pickleOut.close
+
+pickleOut = open("y.pickle", "wb")
+pickle.dump(y, pickleOut)
+pickleOut.close
+
+pickleIn = open("X.pickle", "rb")
+X = pickle.load(pickleIn)
+
+print(X[1])
